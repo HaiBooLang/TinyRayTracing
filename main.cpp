22abfd439d52758fe6c34f63823f862bfa36d58a
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-04-12 23:30:35 +0800

    Quadratic function

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 color ray_color(const ray &r) {
   vec3 unit_direction = unit_vector(r.direction());
   auto t = 0.5 * (unit_direction.y() + 1.0);
-  return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.596, 0.765, 0.475);
+  return (1.0 - t*t) * color(1.0, 1.0, 1.0) + t*t * color(0.596, 0.765, 0.475);
 }
 
 void test() {
