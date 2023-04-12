2f996faa6e2fcd6597ff6eb13878b695eed063c8
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-04-12 17:47:33 +0800

    3.3 Color Utility Functions

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
+#include "color.hpp"
+#include "vec3.hpp"
 #include <Windows.h>
 #include <fstream>
 #include <iostream>
 #include <ostream>
 
+
 void test() {
-  std::ofstream img("img.ppm");
+
+  // Image
+
+  std::ofstream img_out("img.ppm");
 
   constexpr int image_width = 256;
   constexpr int image_height = 256;
 
-  img << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+  // Render
+
+  img_out << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
   for (int j = image_height - 1; j >= 0; --j) {
     std::cerr << "\nScanlines ramaining: " << j << ' ' << std::flush;
@@ -18,16 +26,13 @@ void test() {
       auto g = double(j) / (image_height - 1);
       auto b = 0.5;
 
-      int ir = static_cast<int>(255.999 * r);
-      int ig = static_cast<int>(255.999 * g);
-      int ib = static_cast<int>(255.999 * b);
-
-      img << ir << ' ' << ig << ' ' << ib << '\n';
+      color pixel_color(r, g, b);
+      write_color(img_out, pixel_color);
     }
   }
   std::cerr << "\nDone\n";
 
-  img.close();
+  img_out.close();
 }
 
 int main() {
