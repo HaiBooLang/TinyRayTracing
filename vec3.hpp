263aae690ea9383343e27a7ebb30d663330c8d34
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-04-12 16:46:44 +0800

    3.2 vec3 Utility Functions

--- vec3.hpp
+++ vec3.hpp
@@ -3,6 +3,7 @@
 
 #include <cmath>
 #include <iostream>
+#include <ostream>
 
 using std::sqrt;
 
@@ -47,5 +48,41 @@ public:
 
 using color = vec3;
 using point3 = vec3;
+
+inline std::ostream &operator<<(std::ostream &out, const vec3 &v) {
+  return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+(const vec3 &u, const vec3 &v) {
+  return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vec3 operator-(const vec3 &u, const vec3 &v) {
+  return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &u, const vec3 &v) {
+  return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3 &v) {
+  return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &v, double t) { return t * v; }
+
+inline vec3 operator/(vec3 v, double t) { return (1 / t) * v; }
+
+inline double dot(const vec3 &u, const vec3 &v) {
+  return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
+}
+
+inline vec3 cross(const vec3 &u, const vec3 &v) {
+  return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+              u.e[2] * v.e[0] - u.e[0] * v.e[2],
+              u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline vec3 unit_vector(vec3 v) { return v / v.length(); }
 
 #endif
