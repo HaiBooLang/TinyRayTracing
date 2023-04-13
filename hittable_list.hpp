6018ef4ef20c4676a46540346270dc305b7dae9d
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-04-13 16:38:21 +0800

    6.5 A List of Hittable Objects

---
+++ hittable_list.hpp
@@ -0,0 +1,40 @@
+#ifndef HITTABLE_LIST_H
+#define HITTABLE_LIST_H
+
+#include "hittable.hpp"
+
+#include <memory>
+#include <vector>
+
+class hittable_list : public hittable {
+public:
+    hittable_list() {}
+    hittable_list(std::shared_ptr<hittable> object) { add(object); }
+
+    void clear() { objects.clear(); }
+    void add(std::shared_ptr<hittable> object) { objects.push_back(object); }
+
+    virtual bool hit(const ray &r, double t_min, double t_max,
+                     hit_record &rec) const override;
+
+public:
+    std::vector<std::shared_ptr<hittable>> objects;
+};
+
+bool hittable_list::hit(const ray &r, double t_min, double t_max, hit_record &rec)const{
+    hit_record temp_rec;
+    bool hit_anything = false;
+    auto closest_so_far = t_max;
+
+    for(const auto &object:objects){
+        if(object->hit(r, t_min, t_max, temp_rec)){
+            hit_anything = true;
+            closest_so_far=temp_rec.t;
+            rec = temp_rec;
+        }
+    }
+
+    return hit_anything;
+}
+
+#endif
