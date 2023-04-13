5b457b2196d7e1349e2f1aba80ab087fc5222a7b
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-04-13 18:13:14 +0800

    6.1 Some Random Number Utilities

--- rtweekend.hpp
+++ rtweekend.hpp
@@ -2,8 +2,10 @@
 #define RTWEEKEND_H
 
 #include <cmath>
+#include <random>
 #include <limits>
 #include <memory>
+#include <stdlib.h>
 
 // usings
 
@@ -20,6 +22,20 @@ constexpr double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+// random
+
+inline double random_double() {
+    // Returns a random real in [0,1)
+    static std::uniform_real_distribution<double> distribution(0.0,1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max)
+    return min + (max - min) * random_double();
 }
 
 // common headers
