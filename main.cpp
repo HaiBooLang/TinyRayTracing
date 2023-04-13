8dbc4347d65853887a410d0020ed547ea0467209
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-04-13 20:53:41 +0800

    5 spheres

--- main.cpp
+++ main.cpp
@@ -35,7 +35,11 @@ void test() {
     // World
     hittable_list world;
     world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
-    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+    world.add(make_shared<sphere>(point3(0, -1, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, 1, -1), 0.5));
+    world.add(make_shared<sphere>(point3(1, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(-1, 0, -1), 0.5));
+    
 
     // Camera
     camera cam;
@@ -47,12 +51,12 @@ void test() {
     for (int j = image_height - 1; j >= 0; --j) {
         std::cerr << "\nScanlines ramaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            color pixel_color(0,0,0);
-            for (int s = 0; s<samples_per_pixel; ++s) {
-                auto u = (i+random_double())/(image_width-1);
-                auto v = (j+random_double())/(image_height-1);
+            color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
                 ray r = cam.get_ray(u, v);
-                pixel_color +=ray_color(r, world);
+                pixel_color += ray_color(r, world);
             }
             write_color(img_out, pixel_color, samples_per_pixel);
         }
