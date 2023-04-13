b1cf1a5be09cb36a02d3bca033fedd81dd4191da
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-04-13 22:43:38 +0800

    8.4 Fixing Shadow Acne

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ color ray_color(const ray &r, const hittable &world, int depth) {
         return color(0, 0, 0);
 
     // world
-    if (world.hit(r, 0, infinity, rec)) {
+    if (world.hit(r, 0.001, infinity, rec)) {
         point3 target = rec.p + rec.normal + random_in_unit_shpere();
         return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
     }
