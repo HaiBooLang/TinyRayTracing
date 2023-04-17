fc2522ab217571ccedd6043b423cae6d5f14d895
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-04-17 23:23:11 +0800

    12.2 Generating Sample Rays

--- vec3.hpp
+++ vec3.hpp
@@ -150,4 +150,13 @@ inline vec3 refract(const vec3 &uv, const vec3 &n,
     return r_out_perp + r_out_parallel;
 }
 
+inline vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3(random_double(-1, 1), random_double(-1, 1), 0);
+        if (p.length_squared() >= 1)
+            continue;
+        return p;
+    }
+}
+
 #endif
