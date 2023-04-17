dff65436a4021107e505f3754ca4e9f3d9468411
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-04-17 23:11:10 +0800

    11.2 Positioning and Orienting the Camera

--- camera.hpp
+++ camera.hpp
@@ -6,24 +6,27 @@
 // implements a simple camera using the axis-aligned camera
 class camera {
 public:
-    camera(double vfov, // vertical field-of-view in degrees
+    camera(point3 lookfrom, point3 lookat, vec3 vup,
+           double vfov, // vertical field-of-view in degrees
            double aspect_ratio) {
         auto theta = degrees_to_radians(vfov);
         auto h = tan(theta / 2);
         auto viewport_height = 2.0 * h;
         auto viewport_width = aspect_ratio * viewport_height;
 
-        auto focal_length = 1.0;
+        auto w = unit_vector(lookfrom - lookat);
 
-        origin = point3(0, 0, 0);
-        horizontal = vec3(viewport_width, 0.0, 0.0);
-        vertical = vec3(0.0, viewport_height, 0.0);
-        lower_left_corner =
-            origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+        auto u = unit_vector(cross(vup, w));
+        auto v = cross(w, u);
+
+        origin = lookfrom;
+        horizontal = viewport_width * u;
+        vertical = viewport_height * v;
+        lower_left_corner = origin - horizontal / 2 - vertical / 2 - w;
     }
-    ray get_ray(double u, double v) const {
+    ray get_ray(double s, double t) const {
         return ray(origin,
-                   lower_left_corner + u * horizontal + v * vertical - origin);
+                   lower_left_corner + s * horizontal + t * vertical - origin);
     }
 
 private:
@@ -32,5 +35,4 @@ private:
     vec3 horizontal;
     vec3 vertical;
 };
-
 #endif
