3db91eb274b8fbf53b718f1c74089d5d13b3b9c5
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-04-17 22:13:14 +0800

    10.5 Modeling a Hollow Glass Sphere

--- material.hpp
+++ material.hpp
@@ -71,7 +71,8 @@ public:
         const bool cannot_refract = refraction_ratio * sin_theta > 1.0;
         vec3 direction;
 
-        if (cannot_refract)
+        if (cannot_refract ||
+            reflectance(cos_theta, refraction_ratio) > random_double())
             direction = reflect(unit_direction, rec.normal);
         else
             direction = refract(unit_direction, rec.normal, refraction_ratio);
@@ -82,7 +83,15 @@ public:
     }
 
 public:
-    double ir;
+    double ir; // Index of Refraction
+
+private:
+    static double reflectance(double cosine, double ref_idx) {
+        // Use Schlick's approximation for reflectance.
+        auto r0 = (1 - ref_idx) / (1 + ref_idx);
+        r0 = r0 * r0;
+        return r0 + (1 - r0) * pow((1 - cosine), 5);
+    }
 };
 
 #endif
